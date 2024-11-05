678d8065a5a71f4e646fc351263fc5e225a69cf5
jpandey55 <pandeyjitendra5994@gmail.com>
2024-11-05 12:29:07 -0500

    Added Pizza component

--- 03-pizza-menu/pizza-menu/data.js
+++ 03-pizza-menu/pizza-menu/data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,28 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <h1> Hello React!</h1>
-      <Pizza />
-    </div>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizaa</h2>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>
-);
